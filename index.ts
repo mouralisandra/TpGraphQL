ef02d2514c4b7d3b95f594b4f0605112fcab245c
Sandra Mourali <sandra.mourali@insat.ucar.tn>
2023-05-07 21:32:31 +0100

    Minor modifications

--- index.ts
+++ index.ts
@@ -1,26 +1,17 @@
 import { createServer } from 'node:http'
 import { createYoga } from 'graphql-yoga'
 import { createPubSub } from '@graphql-yoga/subscription'
-import { schema } from './src/schema'
+import { schema } from './src/Schemas/schema'
 import { db } from './src/DataBase/DataBase'
-
-
 type PubSubChannels = {
   CVUpdates
 }
-
-// 2
 const pubSub = createPubSub<PubSubChannels>()
-
-
-
 // Create a Yoga instance with a GraphQL schema.
 const yoga = createYoga({ schema , context : { db , pubSub } })
-
 // Pass it into a server to hook into request handlers.
 const server = createServer(yoga)
-
 // Start the server and you're done!
 server.listen(4000, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
+  console.info('TP GraphQL http://localhost:4000/graphql')
 })
--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -1,9 +1,9 @@
 import { createSchema } from 'graphql-yoga'
 import fs from "fs"
 import path from "path"
-import { Mutation } from './Queries/Mutations'
-import { Subscription } from './Queries/Subscriptions'
-import {Query,Skill, CV } from './Queries/Resolvers'
+import { Mutation } from '../Queries/Mutations'
+import { Subscription } from '../Queries/Subscriptions'
+import {Query,Skill, CV } from '../Queries/Resolvers'
  
 export const schema = createSchema({
   typeDefs: fs.readFileSync(path.join(__dirname,"schemas/schema.graphql"),"utf-8"),
